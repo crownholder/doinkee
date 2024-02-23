2916221cd001f3451a8dfb20380328b2d4bce93d
Michael Kees <michael.kees22@yahoo.com>
2024-02-23 13:10:44 -0800

    middleware.ts

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -16,12 +16,12 @@ const Home = async ({ searchParams }: SearchParamProps) => {
         <h1 className="home-heading">
          DOINKEE
         </h1>
-        <ul className="flex-center justify-center w-full gap-20">
+        <ul className="flex-center w-full gap-20">
           {navLinks.slice(1, 5).map((link) => (
             <Link
               key={link.route}
               href={link.route}
-              className="flex-center flex-col gap-2"
+              className="flex-center flex-col gap-3"
             >
               <li className="flex-center w-fit rounded-full bg-black p-4">
                 <Image src={link.icon} alt="image" width={24} height={24} />
